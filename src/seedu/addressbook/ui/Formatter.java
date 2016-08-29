c49ba68a9e81e113cd3115242d8b950b5262c124
zhiyong4113 <zhiyong.ong@hotmail.com>
2016-08-30 01:15:22 +0800

    Refactored TextUi class. Abstracted out methods to do with formatting the displayed string.

---
+++ src/seedu/addressbook/ui/Formatter.java
@@ -0,0 +1,26 @@
+package seedu.addressbook.ui;
+
+import java.util.List;
+
+public class Formatter {
+	
+	/** Formats a list of strings as a viewable indexed list. */
+    public static String getIndexedListForViewing(List<String> listItems) {
+        final StringBuilder formatted = new StringBuilder();
+        int displayIndex = 0 + TextUi.DISPLAYED_INDEX_OFFSET;
+        for (String listItem : listItems) {
+            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
+            displayIndex++;
+        }
+        return formatted.toString();
+    }
+    /**
+     * Formats a string as a viewable indexed list item.
+     *
+     * @param visibleIndex visible index for this listing
+     */
+    private static String getIndexedListItem(int visibleIndex, String listItem) {
+        return String.format(TextUi.MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+    }
+
+}
